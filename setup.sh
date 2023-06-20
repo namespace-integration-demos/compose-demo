c27e43bf9354fb924963c7b6e61dca3548c8adec
Hugo Santos <hugo@namespacelabs.com>
2023-06-20 11:43:28 +0200

    use a shared remote builder

--- setup.sh
+++ setup.sh
@@ -1,10 +1,25 @@
+set -ex
+
 nsc docker login --output_registry_to /tmp/registry
 REGISTRY=`cat /tmp/registry`
-nsc create --bare --cidfile /tmp/cid --features EXP_DOCKER_CONTAINERD_STORE --experimental_from prewarm.json
+#nsc create --bare --cidfile /tmp/cid --features EXP_DOCKER_CONTAINERD_STORE --experimental_from prewarm.json
+nsc create --bare --cidfile /tmp/cid
 CLUSTER_ID=`cat /tmp/cid`
-nsc docker remote $CLUSTER_ID compose up --build -d
+DOCKER_CONTEXT=nsc-$CLUSTER_ID
+# Create a separate docker context to ensure that we can change `buildx`'s builder.
+nsc docker attach-context --name $DOCKER_CONTEXT --to $CLUSTER_ID --background
+# Setup the remote builder (for caching across runs).
+nsc docker buildx setup --background --create_at_startup --name nsc-remote
+# Tell buildx to use the remote builder ("nsc-remote").
+docker -c $DOCKER_CONTEXT buildx use nsc-remote
+# Bring up the dependencies.
+docker -c $DOCKER_CONTEXT compose up --build -d
+# Expose the backend's ingress.
 nsc expose $CLUSTER_ID --all --ingress '*=noauth' -o json > ingress.json
 BACKEND=`cat ingress.json | jq -r '.[0].url'`
-nsc build -t $REGISTRY/frontend --build-arg NEXT_PUBLIC_BACKEND_URL=$BACKEND --push frontend
-nsc docker remote $CLUSTER_ID run -d --name frontend -p 3000:3000 $REGISTRY/frontend
+# Issue a build of the frontend.
+docker -c $DOCKER_CONTEXT build -t $REGISTRY/frontend --build-arg NEXT_PUBLIC_BACKEND_URL=$BACKEND --load frontend
+# Start the frontend in the same cluster, alongside the other containers.
+docker -c $DOCKER_CONTEXT run -d --name frontend -p 3000:3000 $REGISTRY/frontend
+# Expose the frontend to the internet.
 nsc expose $CLUSTER_ID --container frontend
